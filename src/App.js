3fb1cbff87c77f5b51e061fb73b4bd697d76f08b
Rajat Kumar <rajat9258@gmail.com>
2023-10-28 14:06:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
 
 function App() {
+
+  const [inputText, setInputText] = useState('');
+  const [city, setCity] = useState('Patna');
+  const [temp, setTemp] = useState('');
+  const [humid, setHumidity] = useState('');
+  const [visible, setVisibility] = useState('');
+  const [wind, setWindSpeed] = useState('');
+
+
+  useEffect(() => {
+    async function fetchAPI() {
+      try {
+        const API_key = process.env.REACT_APP_API_KEY;
+        const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`);
+        console.log(response.data);
+
+
+        setTemp(response.data.main.temp);
+        setHumidity(response.data.main.humidity);
+        setVisibility(response.data.visibility);
+        setWindSpeed(response.data.wind.speed);
+
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchAPI();
+  }, [city]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" onChange={(event) => { setInputText(event.target.value) }} value={inputText} />
+      <button onClick={() => { setCity(inputText); }}>Submit</button>
+
+      <h2 style={{ textTransform: 'capitalize' }}> City: {city}</h2>
+      <h2> Temperature: {temp} °C </h2>
+      <h2>Humidity: {humid}</h2>
+      <h2>Visibility: {visible}</h2>
+      <h2>Wind Speed: {wind}</h2>
+
+    </>
   );
 }
 
