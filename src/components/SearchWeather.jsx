36388eea76e22eaa089b83506bf5cbcbfd067fa5
Rajat Kumar <rajat9258@gmail.com>
2023-10-31 22:16:01 +0530

    Reduce useState

--- src/components/SearchWeather.jsx
+++ src/components/SearchWeather.jsx
@@ -1,18 +1,11 @@
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const SearchWeather = () => {
 
-    
     const [inputText, setInputText] = useState('');
     const [city, setCity] = useState('Patna');
-    const [temp, setTemp] = useState('');
-    const [humid, setHumidity] = useState('');
-    const [visible, setVisibility] = useState('');
-    const [wind, setWindSpeed] = useState('');
-    const [sky, setSky] = useState('');
-
+    const [weatherData, setWeatherData] = useState(null);
 
     useEffect(() => {
         async function fetchAPI() {
@@ -20,14 +13,8 @@ const SearchWeather = () => {
                 const API_key = process.env.REACT_APP_API_KEY;
                 const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`);
                 console.log(response.data);
-               
-                console.log(response.data.weather[0].main);
 
-                setTemp(response.data.main.temp);
-                setHumidity(response.data.main.humidity);
-                setVisibility(response.data.visibility);
-                setWindSpeed(response.data.wind.speed);
-                setSky(response.data.weather[0].main);
+                setWeatherData(response.data);
 
             } catch (error) {
                 console.error(error);
@@ -41,6 +28,34 @@ const SearchWeather = () => {
         setCity(inputText);
     }
 
+    let emoji = null;
+    if (weatherData && weatherData.main) {
+        const mainWeather = weatherData.weather[0].main;
+        switch (mainWeather) {
+            case "Clouds":
+                emoji = "fa-cloud";
+                break;
+            case "Thunderstorm":
+                emoji = "fa-bolt";
+                break;
+            case "Drizzle":
+                emoji = "fa-cloud-rain";
+                break;
+            case "Rain":
+                emoji = "fa-cloud-shower-heavy";
+                break;
+            case "Snow":
+                emoji = "fa-cloud-snow-flake";
+                break;
+            default:
+                emoji = "fa-smog";
+                break;
+        }
+    } else {
+        return (
+            <div>...Loading</div>
+        );
+    }
     // Date
     let d = new Date();
     let date = d.getDate();
@@ -61,7 +76,9 @@ const SearchWeather = () => {
                 <div className="row justify-content-center">
                     <div className="col-md-4">
                         <div className="card text-white text-center border-0">
-                            <img src={`https://source.unsplash.com/600x900/?${sky}`} className="card-img" alt="..."  />
+                            {weatherData && ( // Check if weatherData is not null
+                                <img src={`https://source.unsplash.com/600x900/?${weatherData.weather[0].main}`} className="card-img" alt="..." />
+                            )}
                             <div className="card-img-overlay">
                                 <form onSubmit={handleSubmit}>
                                     <div className="input-group mb-4 w-75 mx-auto">
@@ -73,35 +90,36 @@ const SearchWeather = () => {
                                         </button>
                                     </div>
                                 </form>
-                                <div className="bg-dark bg-opacity-50 py-3">
-                                    <h5 className="card-title" style={{ textTransform: 'capitalize' }}>{city}</h5>
-                                    <p className="card-text lead">{day}, {month} {date}, {year}
-                                        <br />
-                                        {time}
-                                    </p>
-                                    <hr />
-                                    <i className="fas fa-cloud fa-4x" />
-                                    <h1 className="fw-border mb-3"> {temp} &deg;C</h1>
-                                    <p className="lead fw-bolder mb-8">{sky}</p>
+                                {weatherData && ( // Check if weatherData is not null
+                                    <div className="bg-dark bg-opacity-50 py-3">
+                                        <h5 className="card-title">{city}</h5>
+                                        <p className="card-text lead">{day}, {month} {date}, {year}
+                                            <br />
+                                            {time}
+                                        </p>
+                                        <hr />
+                                        <i className={`fas ${emoji} fa-4x`} />
+                                        <h1 className="fw-border mb-3"> {weatherData.main.temp} &deg;C</h1>
+                                        <p className="lead fw-bolder mb-8">{weatherData.weather[0].main}</p>
 
-                                    <div class="container">
-                                        <div class="row">
-                                            <div class="col">
-                                                <div className="colHeading"> Humidity</div>
-                                                <p>{humid}%</p>
-                                            </div>
-                                            <div class="col">
-                                                <div className="colHeading"> Visibility</div>
-                                                <p>{visible}</p>
-                                            </div>
-                                            <div class="col">
-                                                <div className="colHeading"> Wind</div>
-                                                <p>{wind}</p>
+                                        <div className="container">
+                                            <div className="row">
+                                                <div className="col">
+                                                    <div className="colHeading"> Humidity</div>
+                                                    <p>{weatherData.main.humidity}%</p>
+                                                </div>
+                                                <div className="col">
+                                                    <div className="colHeading"> Visibility</div>
+                                                    <p>{weatherData.visibility}</p>
+                                                </div>
+                                                <div className="col">
+                                                    <div className="colHeading"> Wind</div>
+                                                    <p>{weatherData.wind.speed}</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-
-                                </div>
+                                )}
                             </div>
                         </div>
                     </div>
